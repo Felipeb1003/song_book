473e4a0df8c1ac58fbe3801cb908a1043b8fc247
Felipe <felipebeltran.nyc@gmail.com>
2020-06-28 03:21:52 -0400

    Setlists Controller

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -1,18 +1,21 @@
 class SetlistsController < ApplicationController
 
-
     get '/setlists' do 
-        @setlists = current_user.setlists
+        check_if_user_logged_in
+        @setlists = Setlist.all
+
         erb :'/setlists/index'
     end    
 
     get '/setlists/new' do 
-        @songs =  current_user.songs
+        check_if_user_logged_in
+        @songs = current_user.songs
        
         erb :'/setlists/new'
     end    
 
     post '/setlists' do 
+        check_if_user_logged_in
         @setlist = Setlist.create(params[:setlist])
         @setlist.user_id = current_user.id
         @setlist.save
@@ -21,8 +24,56 @@ class SetlistsController < ApplicationController
     end    
 
     get '/setlists/:id' do 
-        @setlist = Setlist.find_by_id(params[:id])
-        @songs 
+        set_setlist
+        check_if_user_logged_in
+
         erb :'/setlists/show'
     end   
+
+    get '/setlists/:id/edit' do 
+        check_if_user_logged_in
+        @setlist = Setlist.find_by_id(params[:id])
+
+        if current_user == @setlist.user
+            @songs = current_user.songs
+            erb :'/setlists/edit'
+        else 
+            redirect to "/users/#{current_user.id}"
+        end        
+    end   
+
+    patch '/setlists/:id' do 
+        check_if_user_logged_in
+        @setlist = Setlist.find_by_id(params[:id])
+        
+
+        if current_user == @setlist.user
+            @setlist.update(params[:setlist])
+            @setlist.save
+        
+            redirect to "/setlists/#{@setlist.id}"
+        else   
+            redirect to "/users/#{current_user.id}"
+        end     
+    end
+    
+    delete '/setlists/:id' do 
+        check_if_user_logged_in
+        @setlist = Setlist.find_by_id(params[:id])
+        
+        if current_user == @setlist.user
+            @setlist.delete
+
+            redirect to "/users/#{current_user.id}"
+        else
+            redirect to '/setlists'
+        end        
+    end    
+
+    private
+
+    def set_setlist
+        @setlist = Setlist.find_by(id: params[:id])
+        redirect to '/' if !@setlist
+    end 
 end    
