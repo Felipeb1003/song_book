5320498db60242c7f18c1aecc44eab37d1e05b3f
Felipe <felipebeltran.nyc@gmail.com>
2020-06-28 03:20:23 -0400

    Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,25 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, ENV['TOP_SECRET']
+    set :show_exceptions, false
   end
+
+  not_found do 
+   status 404
+
+   erb :error
+  end  
 
   get "/" do
     erb :index
   end
 
+  error ActiveRecord::RecordNotFound do
+   redirect to '/'
+  end  
+
   helpers do 
-    def is_loggged_in?
+    def is_logged_in?
       !!session[:user_id]
     end
     
@@ -22,7 +33,12 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
     end  
 
-    
+    def check_if_user_logged_in
+      if is_logged_in?
+      else
+        redirect to '/users/login'
+      end
+    end  
   end    
-    
+  
 end
