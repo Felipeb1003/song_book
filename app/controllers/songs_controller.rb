d770dd2348603976452e75fd21b9acd8e59e3aef
Felipe <felipebeltran.nyc@gmail.com>
2020-06-28 03:21:31 -0400

    Songs Controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,20 @@
 class SongsController < ApplicationController
 
     get '/songs' do 
-        @songs = current_user.songs
+        check_if_user_logged_in
+        @songs = Song.all
+
         erb :'/songs/index'
     end    
     
     get '/songs/new' do 
+        check_if_user_logged_in
+
         erb :'/songs/new'
     end    
 
     post '/songs' do 
-       
+        check_if_user_logged_in
         @song = Song.create(params[:song])
         @song.user_id = current_user.id
         @song.save
@@ -19,31 +23,51 @@ class SongsController < ApplicationController
     end    
 
     get '/songs/:id' do 
-        @song = Song.find_by_id(params[:id])
+        set_song
+        check_if_user_logged_in
+
         erb :'/songs/show'
     end   
     
     get '/songs/:id/edit' do 
+        check_if_user_logged_in
         @song = Song.find_by_id(params[:id])
         
-        erb :'/songs/edit'
+        if current_user == @song.user
+            erb :'/songs/edit'
+        else
+            redirect to "/users/#{current_user.id}"
+        end    
     end   
     
     patch '/songs/:id' do 
+        check_if_user_logged_in
         @song = Song.find_by_id(params[:id])
-        @song.update(params[:song])
 
-        @song.save
-       
-        redirect to "/songs/#{@song.id}"
-
+        if current_user == @song.user
+            @song.update(params[:song])
+            @song.save
+            redirect to "/songs/#{@song.id}"
+        else  
+            redirect to "/users/#{current_user.id}"
+        end     
     end    
     
     delete '/songs/:id' do 
+        check_if_user_logged_in
         @song = Song.find_by_id(params[:id])
-        @song.delete
-
-        redirect to '/songs'
-    end    
-
+       
+        if current_user == @song.user
+            @song.delete
+            redirect to "/users/#{current_user.id}"
+        else
+            redirect to '/songs'    
+        end
+    end  
+    
+    private
+    def set_song
+     @song = Song.find_by(id: params[:id])
+     redirect to '/' if !@song
+    end 
 end   
