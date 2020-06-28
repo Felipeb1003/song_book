012414725cf91b5729f30be6c8e7264acfc9f1bb
Felipe <felipebeltran.nyc@gmail.com>
2020-06-28 03:21:10 -0400

    Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,63 @@
 class UsersController < ApplicationController
 
-    get '/users/signup' do 
-        erb :'/users/new'
-    end  
+  get '/users/signup' do 
+    erb :'/users/new'
+  end  
 
-    post '/users/signup' do 
-     
-      user = User.new(params[:user])
-        if user.save
-            session[:user_id] = user.id 
+  post '/users/signup' do 
+    user = User.new(params[:user])
 
-            redirect to '/users/login'
-        else
-           
-            @error = user.errors.full_messages.join("  -  ")
-            erb :'users/new'
-        end
-    end    
+    if user.save
+      session[:user_id] = user.id 
+
+      redirect to '/users/login'
+    else
+      @error = user.errors.full_messages.join("  -  ")
+
+      erb :'users/new'
+    end
+  end    
         
-    get '/users/login' do 
-            erb :'/users/login'
+  get '/users/login' do 
+    if !is_logged_in?
+      erb :'/users/login'
+    else    
+      redirect "/users/#{current_user.id}"
     end    
+  end    
 
-    post '/users/login' do
-     
-      user = User.find_by_username(params[:user][:username])
+  post '/users/login' do
+    user = User.find_by_username(params[:user][:username])
 
-        if user && user.authenticate(params[:user][:password])
-          session[:user_id] = user.id
-          
-          redirect to '/users/:id'
-        else
-            erb :'/users/login'
-        end
-      end
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      
+      redirect "/users/#{current_user.id}"
+    else
+      @error = "Invalid Username or Password..."
+      
+      erb :'/users/login'
+    end
+  end
+      
+  get '/users/logout' do
+    session.clear
+    redirect to '/users/login'
+  end
 
-      get '/users/logout' do
-        session.clear
-        redirect 'users/login'
-      end
+  get '/users/:id' do 
+    set_user
+    check_if_user_logged_in
+    @songs = current_user.songs
+    @setlists = current_user.setlist
 
-      get '/users/:id' do
-        @user = User.find(session[:user_id])
-        @songs = Song.all
-        erb :'/users/show'
-      end
+    erb :'/users/show'
+  end
 
+  private
+   def set_user
+    @user = User.find_by(id: params[:id])
+    
+    redirect to '/' if !@user 
+   end 
 end    
